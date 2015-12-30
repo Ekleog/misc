757743f7f035aaccd3394a2554ed3e4c46fc596b
Léo Gaspard <leo@gaspard.io>
2015-12-30 06:11:22 +0100

    Add a name to operations on stack machine

--- stack_machine/Operation.hpp
+++ stack_machine/Operation.hpp
@@ -1,10 +1,14 @@
 #ifndef OPERATION_HPP_INCLUDED_
 #define OPERATION_HPP_INCLUDED_ 1
 
+#include <string>
+
 class State;
 
 class Operation {
 public:
+    virtual std::string name() const = 0;
+
     virtual void run(State &s) const = 0;
 };
 
--- stack_machine/operations/JumpOp.hpp
+++ stack_machine/operations/JumpOp.hpp
@@ -1,6 +1,8 @@
 #ifndef OPERATIONS_JUMPOP_HPP_INCLUDED_
 #define OPERATIONS_JUMPOP_HPP_INCLUDED_ 1
 
+#include <sstream>
+
 #include "../Operation.hpp"
 #include "../State.hpp"
 
@@ -9,6 +11,12 @@ class JumpOp : public Operation {
 
 public:
     JumpOp(std::size_t dest) : dest_(dest) { }
+
+    virtual std::string name() const {
+        std::ostringstream oss;
+        oss << "jump " << dest_;
+        return oss.str();
+    }
 
     virtual void run(State &s) const {
         s.ip() = dest_;
