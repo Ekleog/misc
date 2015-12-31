2a0b47d9c7ec7fc50440c067a8da5f0fbc78062d
Léo Gaspard <leo@gaspard.io>
2015-12-31 03:02:42 +0100

    Use generating the syracuse series the example for stack machine

--- stack_machine/main.cpp
+++ stack_machine/main.cpp
@@ -8,10 +8,17 @@ int main() {
     Program p(parse(std::istringstream(
         "read\n"
         ":loop\n"
-        "   dup add\n"
-        "   push 1 add\n"
+        "   dup push 2 mod\n"
+        "   jnz :not_multiple_2\n"
+        "       push 2 div"
+        "       jump :end_cond\n"
+        "   :not_multiple_2\n"
+        "       push 3 mul\n"
+        "       push 1 add\n"
+        "   :end_cond\n"
         "   dup print\n"
-        "jump :loop\n"
+        "   dup push 1 sub\n"
+        "jnz :loop\n"
     )));
 
     std::cout << p.to_string();
