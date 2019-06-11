a65013fbf4b180fe5e4c151b8cb44c792b0eeec1
Léo Gaspard <leo@gaspard.io>
2019-06-11 15:06:00 +0200

    wip

---
+++ python/meta_path/almostbar.py
@@ -0,0 +1,7 @@
+print("""
+This has worked iff it has imported :
+- foo
+- bar (under the "almostbar" name)
+- baz
+- almostbar
+""")
---
+++ python/meta_path/meta_path.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import traceback
+from importlib.abc import MetaPathFinder
+from importlib.util import spec_from_file_location
+
+class TestImporter(MetaPathFinder):
+    @classmethod
+    def calling_file(cls):
+        stack = traceback.extract_stack()
+        for f in stack[1:]: # Walk the stack from the frame above this one
+            if f.filename[:8] != '<frozen ':
+                return f.filename
+        return None
+
+    @classmethod
+    def find_spec(cls, fullname, path=None, target=None):
+        fro = cls.calling_file()
+        print("Trying to import", fullname, "from file {}", fro)
+        print("  details:", cls, fullname, path, target)
+        if fullname == "almostbar":
+            return spec_from_file_location(fullname, os.path.join(os.getcwd(), "bar.py"))
+
+sys.meta_path.insert(0, TestImporter)
+
+import foo
